3863e1a6450b5e4f7700aa813adcf62ccfefbf5e
CDamianS <A01656277@tec.mx>
2022-12-27 09:56:11 -0600

    To Do list added

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbletea"
+	"os"
 )
 
 type status int
@@ -38,8 +41,12 @@ type Model struct {
 	err  error
 }
 
+func New() *Model {
+	return &Model{}
+}
+
 // TODO: call this on tea.WindowsSizeMsg
-func (m *Model) initList(width, height) {
+func (m *Model) initList(width, height int) {
 	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
 	m.list.Title = "To Do"
 	m.list.SetItems([]list.Item{
@@ -47,4 +54,32 @@ func (m *Model) initList(width, height) {
 		Task{status: todo, title: "eat sushi", description: "miso soup too"},
 		Task{status: todo, title: "laundry", description: "or not idc"},
 	})
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.initList(msg.Width, msg.Height)
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+
+}
+
+func (m Model) View() string {
+	return m.list.View()
+}
+
+func main() {
+	m := New()
+	p := tea.NewProgram(m)
+	if err := p.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
